6f42802afe70bc17eb63354d7abcd639bcf477e3
Brian Chu <atuinblue@gmail.com>
2013-04-10 12:19:45 -0700

    Created package and driver class for LootRoller.
    LootRollerLogic java project created, package initialized for Java
    Logic.
    NOTE: use com.BFChuAndGAblett.TreasureSource package for Android project
    files.

---
+++ LootRollerLogic/src/com/BFChuAndGAblett/TreasureSource/Logic/LootLogicDriver.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.BFChuAndGAblett.TreasureSource.Logic;
+
+/**
+ * @author Brian
+ *
+ */
+public class LootLogicDriver {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
