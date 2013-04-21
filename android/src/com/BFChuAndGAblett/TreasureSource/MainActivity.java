a7e80e865d2511d33075b5627e418618dd84704b
Brian Chu <atuinblue@gmail.com>
2013-04-21 12:13:38 -0700

    Added logging calls to the MainActivity() methods
    Log.d() calls were added to onCreate(), onStart(), onResume(),
    onPause(), onStop(), onDestroy(), and onRollLootButton()

--- android/src/com/BFChuAndGAblett/TreasureSource/MainActivity.java
+++ android/src/com/BFChuAndGAblett/TreasureSource/MainActivity.java
@@ -3,6 +3,7 @@ package com.BFChuAndGAblett.TreasureSource;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -12,11 +13,15 @@ import android.view.View;
  * 
  */
 public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onCreate()");
+        }
     }
 
     @Override
@@ -29,35 +34,56 @@ public class MainActivity extends Activity {
     @Override
     public void onStart() {
         super.onStart();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onStart()");
+        }
     }
 
     @Override
     public void onRestart() {
         super.onRestart();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onRestart()");
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onResume()");
+        }
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onPause()");
+        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onStop()");
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onDestroy()");
+        }
     }
 
     public void onRollLootButton(View v) {
         Intent g = new Intent(this, LootDisplay.class);
         startActivity(g);
+        if (BuildConfig.DEBUG) {
+            Log.d(TAG, "in onRollLootButton()");
+        }
     }
 }
