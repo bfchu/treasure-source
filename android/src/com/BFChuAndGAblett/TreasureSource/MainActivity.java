8328ae9ca6ae4fe7d917164e4857099c01f6faf9
Garrick Ablett <ainaeria@gmail.com>
2013-05-12 13:25:18 -0700

    >.< Dumb Import Fix
    imported Radio Button and Checkbox...

--- android/src/com/BFChuAndGAblett/TreasureSource/MainActivity.java
+++ android/src/com/BFChuAndGAblett/TreasureSource/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.CheckBox;
 
 /**
  * @author Brian F. Chu, Garrick S. Ablett
