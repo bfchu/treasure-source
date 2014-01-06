c74895377e6eec144afb4e69c5d25540bece4835
bfchu <atuinblue@gmail.com>
2014-01-06 13:08:08 -0800

    copied relevant files from treasure source into treasuresource.database

---
+++ ts-database-builder/src/com/BFChuAndGAblett/TreasureSource/database/LootItemGoods.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.BFChuAndGAblett.TreasureSource.database;
+
+/**
+ * @author Brian Chu and Garrick Ablett
+ * 
+ */
+public class LootItemGoods extends LootItem {
+    // 1 = gems, 2 = art
+    private int goodsType;
+
+    public LootItemGoods() {
+        super("goods", 1, 10.00, 0, 0, 0, 2);
+        this.goodsType = 1;
+    }
+
+    public LootItemGoods(Integer quantity, Double gValue, Integer numRolled,
+            int goodsType) {
+        super("goods", quantity, gValue, 0, 0, numRolled, 2);
+        this.goodsType = goodsType;
+    }
+
+    public int getGoodsType() {
+        return goodsType;
+    }
+
+    public void setGoodsType(int goodsType) {
+        this.goodsType = goodsType;
+        if (this.goodsType != 2) {
+            this.setName("Gems");
+        } else {
+            this.setName("Art");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LootItemGoods [goodsType=" + goodsType + ", getQuantity()="
+                + getQuantity() + ", getgValue()=" + getgValue()
+                + ", getNumRolled()=" + getNumRolled() + "]";
+    }
+
+}
